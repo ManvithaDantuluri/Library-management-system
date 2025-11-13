4c312923620a079cb6d738926c0062fb8b3b4d08
Yugandhar06 <163870963+Yugandhar06@users.noreply.github.com>
2025-11-13 16:47:21 +0530

    Refactor UserRepository to extend JpaRepository

--- library/src/main/java/com/library/repository/UserRepository.java
+++ library/src/main/java/com/library/repository/UserRepository.java
@@ -1,5 +1,12 @@
 package com.library.repository;
 
-public class UserRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.library.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+    Optional<User> findByUsername(String username);
 }
